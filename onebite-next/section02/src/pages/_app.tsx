dee86f9b2e447b790f19a33ea15b23e8d8683304
신희수 <shs@tigensoft.co.kr>
2026-05-04 13:43:40 +0900

    feat(강의): day3 완료

--- onebite-next/section02/src/pages/_app.tsx
+++ onebite-next/section02/src/pages/_app.tsx
@@ -1,10 +1,35 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push("/test");
+  };
+
+  useEffect(() => {
+    router.prefetch("/test"); // router 방식도 pre fetch 진행
+  }, []);
+
   return (
     <>
-      <header>글로벌 헤더</header>
+      <header>
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        {/* pre fetch 명시적으로 해제 */}
+        <Link href={"/search"} prefetch={false}>
+          search
+        </Link>
+        &nbsp;
+        <Link href={"/book/1"}>book/1</Link>
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
+      </header>
       <Component {...pageProps} />
     </>
   );
