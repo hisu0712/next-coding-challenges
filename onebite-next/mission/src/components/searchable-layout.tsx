b3ca3bee0cf73b1dea920a83ee05c4a34f6c7381
신희수 <shs@tigensoft.co.kr>
2026-05-06 09:46:32 +0900

    feat(미션): day4 완료

---
+++ onebite-next/mission/src/components/searchable-layout.tsx
@@ -0,0 +1,44 @@
+import { useRouter } from "next/router";
+import { ReactNode, useState } from "react";
+import style from "./searchable-layout.module.css";
+
+export default function SearchableLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const q = router.query.q as string;
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!search || q === search) return;
+    router.push(`/search/?q=${search}`);
+  };
+
+  const onkeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div>
+      <div className={style.searchable_container}>
+        <input
+          value={search}
+          onChange={onChangeSearch}
+          onKeyDown={onkeyDown}
+          placeholder="검색어를 입력하세요 ..."
+        />
+        <button onClick={onSubmit}>검색</button>
+      </div>
+      {children}
+    </div>
+  );
+}
--- onebite-next/mission/src/pages/_app.tsx
+++ onebite-next/mission/src/pages/_app.tsx
@@ -1,6 +1,10 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ReactNode } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+
+  return <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>;
 }
