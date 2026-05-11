36506b4cff757b2f33373277000a7ff0f525b6fb
신희수 <shs@tigensoft.co.kr>
2026-05-11 14:26:31 +0900

    feat(미션): vercel 배포 실패. 그외 미션 완료

--- onebite-next/mission/src/pages/index.tsx
+++ onebite-next/mission/src/pages/index.tsx
@@ -5,6 +5,7 @@ import style from "./index.module.css";
 import { InferGetStaticPropsType } from "next";
 import fetchMovies from "@/lib/fetch-movies";
 import fetchRandomMovies from "@/lib/fetch-random-movies";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
   const [allMovies, recoMovies] = await Promise.all([
@@ -22,24 +23,30 @@ export default function Home({
   recoMovies,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className={style.container}>
-      <section className={style.section_recommend}>
-        <h3>지금 가장 추천하는 영화</h3>
-        <div className={style.movie_wrapper}>
-          {recoMovies.slice(0, 3).map((movie) => (
-            <MovieItem key={movie.id} {...movie} />
-          ))}
-        </div>
-      </section>
-      <section className={style.section_all}>
-        <h3>등록된 모든 영화</h3>
-        <div className={style.movie_wrapper}>
-          {allMovies.map((movie) => (
-            <MovieItem key={movie.id} {...movie} />
-          ))}
-        </div>
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>한입시네마</title>
+        <meta property="og:image" content="/thumbnail.png" />
+      </Head>
+      <div className={style.container}>
+        <section className={style.section_recommend}>
+          <h3>지금 가장 추천하는 영화</h3>
+          <div className={style.movie_wrapper}>
+            {recoMovies.slice(0, 3).map((movie) => (
+              <MovieItem key={movie.id} {...movie} />
+            ))}
+          </div>
+        </section>
+        <section className={style.section_all}>
+          <h3>등록된 모든 영화</h3>
+          <div className={style.movie_wrapper}>
+            {allMovies.map((movie) => (
+              <MovieItem key={movie.id} {...movie} />
+            ))}
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
 
