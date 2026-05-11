6f0a45bd03d8013bbedcbd7310ec4fb58d9206cd
신희수 <shs@tigensoft.co.kr>
2026-05-11 18:45:19 +0900

    feat(미션): day9 완료

---
+++ onebite-next/onebite-cinema-app/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,10 @@
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q: string }>;
+}) {
+  const { q } = await searchParams;
+  console.log(searchParams);
+
+  return <div>search 페이지: {q}</div>;
+}
